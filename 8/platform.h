a20f753e2585ef887f7242e7bd8863836df65715
Ori Bernstein <ori@eigenstate.org>
2012-06-19 20:06:43 -0400

    Use the appropriate assembler for OSX.

--- 8/platform.h
+++ 8/platform.h
@@ -1,8 +1,9 @@
 #if defined(__APPLE__) && defined(__MACH__)
 /* for OSX */
+#   define Assembler "as -arch i386 -g -o %s -"
 #   define Fprefix "_"
 #else
 /* Default to linux */
+#   define Assembler "as --32 -g -o %s -"
 #   define Fprefix ""
 #endif
-#define Assembler "as --32 -g -o %s -"
