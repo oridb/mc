3982a9ac9debac6f6c0670f59d3577610b69d4f9
Ori Bernstein <ori@eigenstate.org>
2012-06-21 00:34:36 -0400

    Add mmap/munmap syscalls to test code.

--- test/hello/syscall.s
+++ test/hello/syscall.s
@@ -11,13 +11,22 @@ sys$syscall:
 	.long .a2
 	.long .a3
 	.long .a4
-.a5:	movl 32(%ebp),%edi
-.a4:	movl 28(%ebp),%esi
-.a3:	movl 24(%ebp),%edx
-.a2:	movl 20(%ebp),%ecx
-.a1:	movl 16(%ebp),%ebx
-	/* 12(%ebp) holds nargs */
-.a0:	movl 8(%ebp),%eax
+	.long .a5
+	.long .a5 
+	/*
+	   hack: 6 args uses %ebp, so we index
+	   relative to %esp. This means that if
+	   we actually start using stack space,
+	   this code will need adjustment
+	 */
+.a6:	movl 36(%esp),%ebp
+.a5:	movl 32(%esp),%edi
+.a4:	movl 28(%esp),%esi
+.a3:	movl 24(%esp),%edx
+.a2:	movl 20(%esp),%ecx
+.a1:	movl 16(%esp),%ebx
+	/* 12(%esp) holds nargs */
+.a0:	movl 8(%esp),%eax
         int $0x80
 	movl %ebp,%esp
 	popl %ebp
