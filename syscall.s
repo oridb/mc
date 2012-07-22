42d8c26769b81b40e790cf4de86f86e1c4193841
Ori Bernstein <ori@eigenstate.org>
2012-07-21 20:20:44 -0400

    Compile on OSX

--- syscall.s
+++ syscall.s
@@ -31,3 +31,12 @@ std$syscall:
 	movl %ebp,%esp
 	popl %ebp
 	ret
+
+.globl _std$syscall
+_std$syscall:
+        popl %ecx	/* return address */
+	popl %eax       /* call num */
+	pushl %ecx
+        int $0x80
+	pushl %ecx	/* put the return address back */
+	ret
