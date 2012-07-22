4a5814d74531579135e015781e869ad47bb58c6e
Ori Bernstein <ori@eigenstate.org>
2012-07-21 21:01:33 -0400

    Make system calls work on OSX.
        It's close enough to Linux in terms of system call numbers
        that we can make things work across both.

--- syscall.s
+++ syscall.s
@@ -4,29 +4,23 @@ std$syscall:
 	movl %esp,%ebp
 	movl 12(%ebp),%eax #count
         shl  $2,%eax
-	jmp  *.jmptab(%eax)
-.jmptab:
-	.long .a0
-	.long .a1
-	.long .a2
-	.long .a3
-	.long .a4
-	.long .a5
-	.long .a6
 	/*
 	   hack: 6 args uses %ebp, so we index
-	   relative to %esp. This means that if
-	   we actually start using stack space,
-	   this code will need adjustment
+	   relative to %esp.
+
+           hack: We load 6 args regardless of
+           how many we actually have. This may
+           load junk values, but if the syscall
+           doesn't use them, it's going to be
+           harmless.
 	 */
-.a6:	movl 36(%esp),%ebp
-.a5:	movl 32(%esp),%edi
-.a4:	movl 28(%esp),%esi
-.a3:	movl 24(%esp),%edx
-.a2:	movl 20(%esp),%ecx
-.a1:	movl 16(%esp),%ebx
-	/* 12(%esp) holds nargs */
-.a0:	movl 8(%esp),%eax
+	movl 32(%esp),%ebp
+	movl 28(%esp),%edi
+	movl 24(%esp),%esi
+	movl 20(%esp),%edx
+	movl 16(%esp),%ecx
+	movl 12(%esp),%ebx
+	movl 8(%esp),%eax
         int $0x80
 	movl %ebp,%esp
 	popl %ebp
