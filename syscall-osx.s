29c376fb854767c551415cd858c462f8ac03280d
Ori Bernstein <ori@eigenstate.org>
2012-08-06 11:10:55 -0400

    Make things work on OSX in the new world.

--- syscall-osx.s
+++ syscall-osx.s
@@ -1,8 +1,22 @@
 .globl _std$syscall
 _std$syscall:
-        popq %rcx	/* return address */
-	popq %rax       /* call num */
-	pushq %rcx
-        syscall
-	pushq %rcx	/* put the return address back */
+	pushq %rbp
+	/*
+	hack: We load 6 args regardless of
+	how many we actually have. This may
+	load junk values, but if the syscall
+	doesn't use them, it's going to be
+	harmless.
+	 */
+	movq 16(%rsp),%rax
+	movq 24(%rsp),%rdi
+	movq 32(%rsp),%rsi
+	movq 40(%rsp),%rdx
+	movq 48(%rsp),%r10
+	movq 56(%rsp),%r8
+	movq 64(%rsp),%r9
+
+	syscall
+
+	popq %rbp
 	ret
