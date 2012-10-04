aa736008ef9bec12e7e880f2ce60b2aa0e04260c
Ori Bernstein <ori@eigenstate.org>
2012-10-03 22:00:31 -0400

    Make type freshening work better.

--- parse/infer.c
+++ parse/infer.c
@@ -130,12 +130,17 @@ static Type *tyfreshen(Inferstate *st, Htab *ht, Type *t)
         ret = mktyvar(t->line);
         htput(ht, t->pname, ret);
         return ret;
+    } else if (t->type == Tygeneric) {
+        for (i = 0; i < t->nparam; i++)
+            if (!hthas(ht, t->param[i]->pname))
+                htput(ht, t->param[i]->pname, mktyvar(t->param[i]->line));
+        return mktyname(t->line, t->name, tyfreshen(st, ht, t->sub[0]));
+    } else {
+        ret = tydup(t);
+        for (i = 0; i < t->nsub; i++)
+            ret->sub[i] = tyfreshen(st, ht, t->sub[i]);
+        return ret;
     }
-
-    ret = tydup(t);
-    for (i = 0; i < t->nsub; i++)
-        ret->sub[i] = tyfreshen(st, ht, t->sub[i]);
-    return ret;
 }
 
 static Type *tyspecialize(Inferstate *st, Type *t)
