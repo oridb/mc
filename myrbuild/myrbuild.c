88e09f2323da6f5f3e8b04bfe492fdf53ffdefbc
Ori Bernstein <orib@google.com>
2012-10-24 14:01:37 -0400

    Formatting fix.

--- myrbuild/myrbuild.c
+++ myrbuild/myrbuild.c
@@ -320,7 +320,7 @@ int main(int argc, char **argv)
             case 'M': muse = optarg; break;
             case 'L': ld = optarg; break;
             case 'R': ar = optarg; break;
-            case 'I': 
+            case 'I':
                 lappend(&incpaths, &nincpaths, optarg);
                 break;
             case 'h':
