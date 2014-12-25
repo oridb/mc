1ee4f4207111b7fcfb0a75db1ebd7298af671007
Ori Bernstein <ori@eigenstate.org>
2014-12-24 19:10:02 -0800

    Make a hello world using syscalls work on Plan 9.
        YAY!

--- libstd/syscall+plan9-x64.s
+++ libstd/syscall+plan9-x64.s
@@ -10,38 +10,39 @@ Luckily, the kernel doesn't touch our stack,
 and we have 256 bytes of gap if we get a note.
 */
 TEXT sys$syscall+0(SB),1,$0
-        MOVQ BX,-16(SP)
-        MOVQ CX,-24(SP)
-        MOVQ DX,-32(SP)
-        MOVQ SI,-40(SP)
-        MOVQ DI,-48(SP)
-        MOVQ BP,-56(SP)
-        MOVQ R8,-64(SP)
-        MOVQ R9,-72(SP)
-        MOVQ R10,-80(SP)
-        MOVQ R11,-88(SP)
-        MOVQ R12,-104(SP)
-        MOVQ R13,-112(SP)
-        MOVQ R14,-120(SP)
-        MOVQ R15,-128(SP)
-        ADDQ $8,SP
+	MOVQ	BX,-16(SP)
+	MOVQ	CX,-24(SP)
+	MOVQ	DX,-32(SP)
+	MOVQ	SI,-40(SP)
+	MOVQ	DI,-48(SP)
+	MOVQ	BP,-56(SP)
+	MOVQ	R8,-64(SP)
+	MOVQ	R9,-72(SP)
+	MOVQ	R10,-80(SP)
+	MOVQ	R11,-88(SP)
+	MOVQ	R12,-104(SP)
+	MOVQ	R13,-112(SP)
+	MOVQ	R14,-120(SP)
+	MOVQ	R15,-128(SP)
+	MOVQ	8(SP),RARG
+	ADDQ	$8,SP
 
 	SYSCALL
 
-	SUBQ $8,SP
-        MOVQ -16(SP),BX
-        MOVQ -24(SP),CX
-        MOVQ -32(SP),DX
-        MOVQ -40(SP),SI
-        MOVQ -48(SP),DI
-        MOVQ -56(SP),BP
-        MOVQ -64(SP),R8
-        MOVQ -72(SP),R9
-        MOVQ -80(SP),R10
-        MOVQ -88(SP),R11
-        MOVQ -104(SP),R12
-        MOVQ -112(SP),R13
-        MOVQ -120(SP),R14
-        MOVQ -128(SP),R15
+	SUBQ	$8,SP
+	MOVQ	-16(SP),BX
+	MOVQ	-24(SP),CX
+	MOVQ	-32(SP),DX
+	MOVQ	-40(SP),SI
+	MOVQ	-48(SP),DI
+	MOVQ	-56(SP),BP
+	MOVQ	-64(SP),R8
+	MOVQ	-72(SP),R9
+	MOVQ	-80(SP),R10
+	MOVQ	-88(SP),R11
+	MOVQ	-104(SP),R12
+	MOVQ	-112(SP),R13
+	MOVQ	-120(SP),R14
+	MOVQ	-128(SP),R15
 	RET
 
