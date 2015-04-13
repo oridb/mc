7a1dfce1281c992e0163a4c71bea66152d827333
Ori Bernstein <ori@eigenstate.org>
2015-04-13 09:59:29 -0700

    Fix sysselect.sh
        Oops, let's not fuck up the prefixes.

--- sysselect.sh
+++ sysselect.sh
@@ -15,32 +15,29 @@ fi
 
 uname_arch=`uname -m`
 if test $uname_arch = "x86_64"; then
-	arch=x64
+	arch="x64"
 fi
 
+# check for system prefixes on .myr src
 if test -f "$1+$sys-$arch.myr"; then
 	echo "$1+$sys-$arch.myr"
 elif test -f $1+$posixy-$arch.myr; then
 	echo $1+$posixy-$arch.myr
 elif test -f "$1+$sys.myr"; then
-	echo "$1+$sys-$arch.myr"
+	echo "$1+$sys.myr"
 elif test -f $1+$posixy.myr; then
-	echo $1+$posixy-$arch.myr
-elif test -f "$1+$sys.myr"; then
-	echo "$1+$sys-$arch.myr"
-elif test -f $1+$posixy.myr; then
-	echo $1+$posixy-$arch.myr
-
+	echo $1+$posixy.myr
+elif test -f $1+$arch; then
+	echo $1+$arch.myr
+# check for system prefixes on .s src
 elif test -f "$1+$sys-$arch.s"; then
 	echo "$1+$sys-$arch.s"
 elif test -f $1+$posixy-$arch.s; then
 	echo $1+$posixy-$arch.s
 elif test -f "$1+$sys.s"; then
-	echo "$1+$sys-$arch.s"
+	echo "$1+$sys.s"
 elif test -f $1+$posixy.s; then
-	echo $1+$posixy-$arch.s
-elif test -f "$1+$sys.s"; then
-	echo "$1+$sys-$arch.s"
-elif test -f $1+$posixy.s; then
-	echo $1+$posixy-$arch.s
+	echo $1+$posixy.s
+elif test -f $1+$arch; then
+	echo $1+$arch.s
 fi
