f366166b75c22f674c6b8b5129cdf127608acc81
Ori Bernstein <ori@eigenstate.org>
2015-04-14 17:33:24 -0700

    Get a more or less working getcwd under OSX.
        TODO: It still truncates long paths.

--- mbldwrap.sh
+++ mbldwrap.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export MYR_MUSE=../muse/muse
+export MYR_MC=../6/6m
+export MYR_RT=../rt/_myrrt.o
+
 # this should be a bourne compatible shell script.
 if test -f mbld/mbld; then
 	./mbld/mbld $@
