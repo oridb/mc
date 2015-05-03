9d653c1ec47061e3985be018dfe29ad93a657ec1
Ori Bernstein <ori@eigenstate.org>
2015-05-03 10:57:20 -0700

    Run gen/cmd commands in the right subdir.

--- mbldwrap.sh
+++ mbldwrap.sh
@@ -5,8 +5,8 @@ export MYR_MC=../6/6m
 export MYR_RT=../rt/_myrrt.o
 
 # this should be a bourne compatible shell script.
-if test -f mbld/mbld; then
-	mbld $@
+if test -f ./mbld/mbld; then
+	./mbld/mbld $@
 else
 	./bootstrap.sh
 fi
