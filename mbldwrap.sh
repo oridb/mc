e686640da8441d0cae0e45d5d6ef9f96c45a8e24
Ori Bernstein <ori@eigenstate.org>
2015-05-14 23:29:49 -0700

    Fix plan9 build.

--- mbldwrap.sh
+++ mbldwrap.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 
-export MYR_MUSE=../muse/muse
-export MYR_MC=../6/6m
-export MYR_RT=../rt/_myrrt.o
-
 # this should be a bourne compatible shell script.
 if test -f ./mbld/mbld; then
 	./mbld/mbld $@
+elif test `uname` = Plan9; then
+	echo "PLAN 9 BOOTSTRAP"
+	export MYR_MUSE=../muse/$O.out
+	export MYR_MC=../$O/$O.out
+	export MYR_RT=../rt/_myrrt.$O
+	./bootstrap9.rc
 else
+	echo "POSIX BOOTSTRAP"
+	export MYR_MUSE=../muse/muse
+	export MYR_MC=../6/6m
+	export MYR_RT=../rt/_myrrt.o
 	./bootstrap.sh
 fi
