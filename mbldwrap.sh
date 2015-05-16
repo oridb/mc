9fa7e376e33410e78fda13897bf8e0914f201ddf
Ori Bernstein <ori@eigenstate.org>
2015-05-16 01:24:34 -0700

    Remove status arg from fatal.
        We never set it to anything other than '1' anyways. There's
        no standardized error codes, so the exit status isn't really
        useful in any case.

--- mbldwrap.sh
+++ mbldwrap.sh
@@ -2,16 +2,20 @@
 
 # this should be a bourne compatible shell script.
 if test `uname` = Plan9; then
-	echo "PLAN 9 BOOTSTRAP"
 	export MYR_MUSE=../muse/$O.out
 	export MYR_MC=../$O/$O.out
 	export MYR_RT=../rt/_myrrt.$O
 	BOOT="./mk/bootstrap/bootstrap+`uname -s`-`uname -m`.sh"
 else
-	echo "POSIX BOOTSTRAP"
 	export MYR_MUSE=../muse/muse
 	export MYR_MC=../6/6m
 	export MYR_RT=../rt/_myrrt.o
 	BOOT="./mk/bootstrap/bootstrap+`uname -s`-`uname -m`.sh"
 fi
-mbld $@ || ./mbld/mbld $@ || $BOOT
+
+if [ -z "$@" ]; then
+    mbld || ./mbld/mbld || $BOOT
+else
+    mbld $@ || ./mbld/mbld $@ || \
+        (echo "Unable to run mbld $@; have you build successfully"; false)
+fi
