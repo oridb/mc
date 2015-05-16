42230832997d2092cad72a775e738ab2d50df1dc
Ori Bernstein <ori@eigenstate.org>
2015-05-15 18:52:31 -0700

    Improve the bootstrap script.
        Use the installed mbld in preference over mbld, and exit
        with the correct status.

--- mbldwrap.sh
+++ mbldwrap.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 # this should be a bourne compatible shell script.
-if test -f ./mbld/mbld; then
-	./mbld/mbld $@
-elif test `uname` = Plan9; then
+if test `uname` = Plan9; then
 	echo "PLAN 9 BOOTSTRAP"
 	export MYR_MUSE=../muse/$O.out
 	export MYR_MC=../$O/$O.out
 	export MYR_RT=../rt/_myrrt.$O
-	./mk/bootstrap/bootstrap+`uname -s`-`uname -m`.sh
+	BOOT="./mk/bootstrap/bootstrap+`uname -s`-`uname -m`.sh"
 else
 	echo "POSIX BOOTSTRAP"
 	export MYR_MUSE=../muse/muse
 	export MYR_MC=../6/6m
 	export MYR_RT=../rt/_myrrt.o
-	./mk/bootstrap/bootstrap+`uname -s`-`uname -m`.sh
+	BOOT="./mk/bootstrap/bootstrap+`uname -s`-`uname -m`.sh"
 fi
-exit 0
+mbld || ./mbld/mbld || $BOOT
