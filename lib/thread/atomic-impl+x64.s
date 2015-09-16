28c95a0761154c121b49e54912f0c45dc7dffec0
Ori Bernstein <ori@eigenstate.org>
2015-09-16 09:32:19 -0700

    Add more atomics for pointer-sized things.

--- lib/thread/atomic-impl+x64.s
+++ lib/thread/atomic-impl+x64.s
@@ -3,7 +3,9 @@ thread$xget32:
 	movl	(%rdi), %eax
 	ret
 .globl thread$xget64
+.globl thread$xgetp
 thread$xget64:
+thread$xgetp:
 	movq	(%rdi), %rax
 	ret
 .globl thread$xset32
@@ -11,7 +13,9 @@ thread$xset32:
 	movl	%esi, (%rdi)
 	ret
 .globl thread$xset64
+.globl thread$xsetp
 thread$xset64:
+thread$xsetp:
 	movq	%rsi, (%rdi)
 	ret
 .globl thread$xadd32
@@ -19,7 +23,9 @@ thread$xadd32:
 	lock xaddl	%esi, (%rdi)
 	ret
 .globl thread$xadd64
+.globl thread$xaddp
 thread$xadd64:
+thread$xaddp:
 	lock xaddq	%rsi, (%rdi)
 	ret
 .globl thread$xsub32
@@ -27,7 +33,9 @@ thread$xsub32:
 	lock xaddl	%esi, (%rdi)
 	ret
 .globl thread$xsub64
+.globl thread$xsubp
 thread$xsub64:
+thread$xsubp:
 	lock xaddq	%rsi, (%rdi)
 	ret
 .globl thread$xcas32
@@ -36,7 +44,9 @@ thread$xcas32:
 	lock cmpxchgl	%edx, (%rdi)
 	ret
 .globl thread$xcas64
+.globl thread$xcasp
 thread$xcas64:
+thread$xcasp:
 	movq	%rsi, %rax
 	lock cmpxchgq	%rdx, (%rdi)
 	ret
@@ -46,7 +56,9 @@ thread$xchg32:
 	lock xchgl	(%rdi), %eax
 	ret
 .globl thread$xchg64
+.globl thread$xchgp
 thread$xchg64:
+thread$xchgp:
 	movq	%rsi, %rax
 	lock xchgq	(%rdi), %rax
 	ret
