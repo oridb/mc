fe9e325f966ff11bb7311e51330ff812362cb41a
Ori Bernstein <ori@eigenstate.org>
2015-11-28 00:44:14 -0800

    Correctly handle spawning and exiting threads.
    	We now unmap the stack correctly on exit. We still
    	have no good thread.exit() all.

---
+++ lib/thread/exit+linux-x64.s
@@ -0,0 +1,22 @@
+/*
+const thread.exit	: (stacksz : std.size -> void)
+NOTE: must be called from the bottom of the stack, since
+we assume that %rbp is in the top 4k of the stack.
+*/
+.globl thread$exit
+thread$exit:
+	/* find top of stack */
+	movq	%rbp,%rdi	/* addr */
+	andq	$~0xfff,%rdi	/* align it */
+	addq	$0x1000,%rdi
+
+	/* syscall args */
+	movq	$11,%rax	/* munmap */
+	movq	-8(%rdi),%rsi	/* size */
+	subq	%rsi,%rdi	/* move to base ptr */
+	syscall
+
+	movq	$60,%rax	/* exit */
+	xorq	%rdi,%rdi	/* 0 */
+	syscall
+	
