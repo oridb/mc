6e064ade4c10bce0127ebebf6cd1dc2bb24213f5
Michael Forney <mforney@mforney.org>
2017-07-04 12:25:39 -0700

    Simplify _start routines
    sys$__environment was unused since 94ee9832f5861c4d09afa12338720eb3a479c342.
    Delete it and count, which is no longer needed.
    
    Simplify _start routines since they no longer need to allocate space for the
    environment slice and populate it.

--- rt/start-freebsd.s
+++ rt/start-freebsd.s
@@ -1,45 +1,37 @@
 .data
-
+/* sys.__cenvp : byte## */
 .globl sys$__cenvp
 sys$__cenvp:
-.quad 0
+    .quad 0
 
 .text
 /*
  * The entry point for the whole program.
  * This is called by the OS. In order, it:
  *  - Sets up all argc entries as slices
- *  - Sets up all envp entries as slices
  *  - Converts argc/argv to a slice
- *  - Stashes envp in std._environment
  *  - Stashes a raw envp copy in __cenvp (for syscalls to use)
  *  - Calls main()
  */
 .globl _start
 _start:
-	/* stack allocate sizeof(byte[:])*(argc + len(envp)) */
-	movq	(%rdi),%rax
-	leaq	16(%rdi,%rax,8), %rbx	/* argp = argv + 8*argc + 8 */
-	call    count
-	addq	%r9,%rax
-	imulq	$16,%rax
-	subq	%rax,%rsp
-	movq	%rsp, %rdx	/* saved args[:] */
+	/* load argc, argv, envp from stack */
+	movq	(%rdi),%rax		/* argc */
+	leaq	8(%rdi),%rbx		/* argv */
+	leaq	16(%rdi,%rax,8),%rcx	/* envp = argv + 8*argc + 8 */
 
-	/* convert envp to byte[:][:] for std._environment */
-	movq	(%rdi),%rax
-	leaq	16(%rdi,%rax,8), %rbx	/* envp = argv + 8*argc + 8 */
-	/* store envp for some syscalls to use without spurious conversion. */
-	movq    %rbx,sys$__cenvp(%rip)
+	/* store envp for some syscalls to use without converting */
+	movq    %rcx,sys$__cenvp(%rip)
+
+	/* stack allocate sizeof(byte[:])*argc */
+	imulq	$16,%rax,%rdx
+	subq	%rdx,%rsp
+	movq	%rsp,%rcx		/* args[:] */
 
 	/* convert argc, argv to byte[:][:] for args. */
-	movq	%rsp, %rcx
-	movq	(%rdi), %rax	/* argc */
-	leaq	8(%rdi), %rbx	/* argv */
-	movq	(%rdi), %rsi	/* saved argc */
-	call    cvt
-	pushq   %rsi
-	pushq   %rdx
+	pushq	%rax
+	pushq	%rcx
+	call	cvt
 
 	xorq %rbp,%rbp
 	/* call pre-main initializers */
@@ -51,4 +43,3 @@ _start:
 	xorq	%rdi,%rdi
 	movq	$1,%rax
 	syscall
-
