f3265a99202d98f6f7e5969e34faae934ce754b9
S. Gilles <sgilles@math.umd.edu>
2018-03-20 09:55:04 -0400

    Fix corrupted merge of 'autoexpr'

--- 6/isel.c
+++ 6/isel.c
@@ -939,7 +939,6 @@ selexpr(Isel *s, Node *n)
 		/* These operators should never show up in the reduced trees,
 		 * since they should have been replaced with more primitive
 		 * expressions by now */
-<<<<<<< HEAD
 	case Obad: case Oauto: case Opreinc: case Opostinc: case Opredec:
 	case Otern: case Opostdec: case Olor: case Oland: case Oaddeq:
 	case Osubeq: case Omuleq: case Odiveq: case Omodeq: case Oboreq:
