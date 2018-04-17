d25b147ccbeff5b7f086b2e43d835543aaa8e6dd
S. Gilles <sgilles@math.umd.edu>
2018-04-17 19:38:45 -0400

    Boilerplate FP control word code for ASM round.

--- lib/math/round-impl+posixy-x64-sse4.s
+++ lib/math/round-impl+posixy-x64-sse4.s
@@ -2,13 +2,31 @@
 .globl math$_rn32
 math$rn32:
 math$_rn32:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	fnstcw	(%rsp)
+	fnstcw	8(%rsp)
+	andl	$0xf3ff, 8(%rsp)
+	fldcw	8(%rsp)
 	cvtss2si	%xmm0, %eax
+	fldcw	(%rsp)
+	leave
 	ret
 
 .globl math$rn64
 .globl math$_rn64
 math$rn64:
 math$_rn64:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	fnstcw	(%rsp)
+	fnstcw	8(%rsp)
+	andl	$0xf3ff, 8(%rsp)
+	fldcw	8(%rsp)
 	cvtsd2si	%xmm0, %rax
+	fldcw	(%rsp)
+	leave
 	ret
 
